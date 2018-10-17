e3a52109a5df3b41a2048f7793d987a1b196593c
Daniel Patrick <danielpatrick@hotmail.com>
2018-10-17 22:11:24 +0100

    Reinstall admin module

--- beerfest/admin.py
+++ beerfest/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Bar, Brewery, Beer, UserBeer
+
+
+admin.site.register(Bar)
+admin.site.register(Brewery)
+admin.site.register(Beer)
+admin.site.register(UserBeer)
--- vrhbf2018/urls.py
+++ vrhbf2018/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import include, url
+from django.contrib import admin
 from django.urls import path
 
+import registration
+
 from beerfest.views import user_profile
+import beerfest
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('accounts/', include("registration.backends.simple.urls")),
     path('accounts/profile/', user_profile, name='user_profile'),
     path('', include("beerfest.urls")),
