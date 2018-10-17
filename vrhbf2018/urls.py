1e294b20f05aeccf3d4b99f0a95f35c78331ee87
Daniel Patrick <danielpatrick@hotmail.com>
2018-10-17 15:16:15 +0100

    Update project urlconf

--- vrhbf2018/urls.py
+++ vrhbf2018/urls.py
@@ -1,17 +1,6 @@
 from django.conf.urls import include, url
 from django.urls import path
 
-from django.contrib import admin
-admin.autodiscover()
-
-import beerfest.views
-
-# Examples:
-# url(r'^$', 'vrhbr2018.views.home', name='home'),
-# url(r'^blog/', include('blog.urls')),
-
 urlpatterns = [
-    url(r'^$', beerfest.views.index, name='index'),
-    url(r'^db', beerfest.views.db, name='db'),
-    path('admin/', admin.site.urls),
+    path('', include("beerfest.urls")),
 ]
