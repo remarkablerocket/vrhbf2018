125cf174debbe3fe02ffaba4624f7da10904b073
Daniel Patrick <danielpatrick@hotmail.com>
2018-10-17 17:58:32 +0100

    Add beer detail view, improve list view, etc

--- beerfest/urls.py
+++ beerfest/urls.py
@@ -1,8 +1,10 @@
-from django.conf.urls import include, url
+from django.conf.urls import include
 from django.urls import path
 
 import beerfest.views
 
 urlpatterns = [
-    url(r'^$', beerfest.views.beer_list, name='index'),
+    path('', beerfest.views.index, name='index'),
+    path('beers/', beerfest.views.beer_list, name='beer_list'),
+    path('beers/<int:id>', beerfest.views.beer_detail, name='beer_detail'),
 ]
--- beerfest/views.py
+++ beerfest/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 from .models import Beer
+
+
+def index(request):
+    return HttpResponseRedirect(reverse("beer_list"))
+
 
 def beer_list(request):
     beer_list = Beer.objects.order_by("bar__id", "brewery__name", "name")
     context = {"beer_list": beer_list}
-    return render(request, "beerfest/index.html", context)
+    return render(request, "beerfest/beer_list.html", context)
+
+
+def beer_detail(request, id):
+    beer = get_object_or_404(Beer, id=id)
+    context = {"beer": beer}
+    return render(request, "beerfest/beer_detail.html", context)
