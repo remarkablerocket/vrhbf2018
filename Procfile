4dbd5e21732102810ae038f1edbb4aa68c2a2b1a
Daniel Patrick <danielpatrick@hotmail.com>
2018-10-17 14:53:51 +0100

    Remove dummy app and replace dummy project name

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn gettingstarted.wsgi
+web: gunicorn vrhbf2018.wsgi
--- vrhbf2018/urls.py
+++ vrhbf2018/urls.py
@@ -4,14 +4,14 @@ from django.urls import path
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
+import beerfest.views
 
 # Examples:
-# url(r'^$', 'gettingstarted.views.home', name='home'),
+# url(r'^$', 'vrhbr2018.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
+    url(r'^$', beerfest.views.index, name='index'),
+    url(r'^db', beerfest.views.db, name='db'),
     path('admin/', admin.site.urls),
 ]
