2a647ca5aaaf369d63c62c5de85176d00f5e7835
Daniel Patrick <danielpatrick@hotmail.com>
2018-10-18 07:30:24 +0100

    Changes to improve admin display

--- beerfest/models.py
+++ beerfest/models.py
@@ -6,9 +6,22 @@ class Brewery(models.Model):
     name = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["id"]
+
 
 class Bar(models.Model):
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+    class Meta:
+        ordering = ["id"]
 
 
 class Beer(models.Model):
@@ -22,9 +35,25 @@ class Beer(models.Model):
     notes = models.TextField()
 
 
+    def __str__(self):
+        return f"{self.name} by {self.brewery.name}"
+
+
+    class Meta:
+        ordering = ["id"]
+
+
 class UserBeer(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     beer = models.ForeignKey(Beer, on_delete=models.CASCADE)
     starred = models.BooleanField(default=True)
     tried = models.BooleanField(default=False)
     rating = models.PositiveSmallIntegerField(null=True, blank=True)
+
+
+    def __str__(self):
+        return f"{self.user.username} and {self.beer.name}"
+
+
+    class Meta:
+        ordering = ["id"]
